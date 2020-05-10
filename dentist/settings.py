bc0637c53e8492cced54cf19f420c94560c9bffe
mrwho95 <dnkh903@gmail.com>
2020-05-11 00:27:44 +0800

    pass value from contact form and return back value to contract html

--- dentist/settings.py
+++ dentist/settings.py
@@ -123,3 +123,11 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
 	os.path.join(BASE_DIR, 'static'),
 ]
+
+# Email Settings
+# EMAIL_HOST = 'localhost'
+# EMAIL_PORT = '1025'
+# EMAIL_HOST_USER = ''
+# EMAIL_HOST_PASSWORD = ''
+# EMAIL_USE_TLS = False
+#EMAIL_USE_SSL = False
--- website/views.py
+++ website/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.core.mail import send_mail
 
 # Create your views here.
 # def home(request):
@@ -14,7 +15,23 @@ def blog(request):
 	return render(request, 'blog.html', {})
 
 def contact(request):
-	return render(request, 'contact.html', {})
+	if request.method == "POST":
+		message_name = request.POST.get('name')
+		message_email = request.POST.get('email')
+		message_content = request.POST.get('content')
+		message_subject = request.POST.get('subject')
+
+		#send an email
+		# send_mail(
+		# 	message_subject, #subject
+		# 	message_content, #content
+		# 	message_email, #from email
+		# 	['dnkh903@gmail.com'], #to email 
+		# 	)
+
+		return render(request, 'contact.html', {'message_name': message_name})
+	else:
+		return render(request, 'contact.html', {})
 
 def department(request):
 	return render(request, 'department.html', {})
