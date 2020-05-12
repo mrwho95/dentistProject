e753e2b297f1d9f7e3a4d47da1f21f2692e4d19c
mrwho95 <dnkh903@gmail.com>
2020-05-12 18:03:49 +0800

    test and add model to Django project

--- website/models.py
+++ website/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Doctor(models.Model):
+	name = models.CharField(max_length = 50)
+	email = models.EmailField(max_length = 100)
+	age = models.IntegerField()
+	department = models.CharField(max_length = 100)
+
+	def __str__(self):
+		return self.name + ' ' + self.email
