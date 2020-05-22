4949e5723117dcb24b7740ceeaf056c54112abb5
mrwho95 <dnkh903@gmail.com>
2020-05-23 07:53:15 +0800

    register staff model to db

--- website/admin.py
+++ website/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import Doctor
+from .models import Doctor, Staff
 from .models import Contact
 
 # Regiser models to admin site
 # Register your models here.
 admin.site.register(Doctor)
 admin.site.register(Contact)
+admin.site.register(Staff)
