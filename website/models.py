11bf3b6c48e95f5c1fe8504243207d4adc69806a
mrwho95 <dnkh903@gmail.com>
2020-05-23 07:54:50 +0800

    add staff model

--- website/models.py
+++ website/models.py
@@ -19,3 +19,16 @@ class Contact(models.Model):
 
 	def __str__(self):
 		return "A new message: "+ self.subject + ' which sent by ' + self.name
+
+class Staff(models.Model):
+	staffID = models.CharField(max_length = 50)
+	name = models.CharField(max_length = 50)
+	email = models.EmailField(max_length = 100)
+	age = models.IntegerField()
+	phone = models.CharField(max_length = 50, null = True)
+	address = models.CharField(max_length = 200)
+	position = models.CharField(max_length = 100)
+	department = models.CharField(max_length = 100)
+
+	def __str__(self):
+		return "Staff "+ self.name
